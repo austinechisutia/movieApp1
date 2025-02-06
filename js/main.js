7ebce94c23b06153951c7f0e18a57909cac0dc8e
austinechisutia <austineakhonya624@gmail.com>
2025-02-06 13:34:21 +0300

    managed to put the images onthe page

--- js/main.js
+++ js/main.js
@@ -9,7 +9,22 @@ $(document).ready(()=>{
 function getMovies(searchText) {
     axios.get(`https://www.omdbapi.com/?apikey=c270fbb2&s=${searchText}`)
         .then((response)=>{
-            console.log(response)
+            console.log(response.data)
+
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie)=>{
+                output += `
+                    <div class="col-md-3 text-centered">
+                        <div class="well">
+                            <img src = "${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class = "btn btn-primary" href"#">Movie Details</a>
+                        </div>
+                    </div>
+                `
+            })
+            $('#movies').html(output)
         })
         .catch((err)=>{
             console.log(err);
