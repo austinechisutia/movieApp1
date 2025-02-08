d65967dcb70ab5fcdceaeb30add4c4db0385b1c1
austinechisutia <austineakhonya624@gmail.com>
2025-02-08 23:52:15 +0300

    well i restarted and here i am managed to use the api

--- js/main.js
+++ js/main.js
@@ -1,52 +1,37 @@
 $(document).ready(()=>{
-    $('#searchForm').on('submit', (e)=>{
-        let searchText = $('#searchText').val();
-        getMovies(searchText);
+    $('#searchForm').on("submit", (e)=>{
+        let searchText = $("#searchText").val()
+        getText(searchText)
         e.preventDefault();
-    })
+    });
 })
 
-function getMovies(searchText) {
+function getText(searchText){
+        console.log($("#searchText").val())
     axios.get(`https://www.omdbapi.com/?apikey=c270fbb2&s=${searchText}`)
-        .then((response)=>{
-            console.log(response.data)
+    .then((response)=>{
+        console.log(response);
 
-            let movies = response.data.Search;
-            let output = '';
-            $.each(movies, (index, movie)=>{
-                output += `
-                    <div class="col-md-3 text-centered">
-                        <div class="well">
-                            <img src = "${movie.Poster}">
-                            <h5>${movie.Title}</h5>
-                            <a onclick="movieSelected('${movie.imdbID}')" class = "btn btn-primary" href"#">Movie Details</a>
-                        </div>
+        let movies = response.data.Search;
+        let output = "";
+
+        $.each(movies, (index, movie)=>{
+            output+=`
+                <div class = "col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick = "movieSelected('${movie.imdbID}')" class="btn btn-primary" htref="#">Movie Details</a>
                     </div>
-                `
-            })
-            $('#movies').html(output)
+                </div>
+            `
         })
-        .catch((err)=>{
-            console.log(err);
-        })
-}
-function movieSelected(id){
-    sessionStorage.setItem('movieId', id);
-    window.location = 'movies.html';
-    return false
-}
-function getMovie(){
-    let movieId = sessionStorage.getItem('movieId');
+        
 
-
-        axios.get(`http://www.omdbapi.com/?apikey=c270fbb2&i=${movieId}`)
-
-        .then((response)=>{
-            console.log(response.data)
-            let movie = response.data;
-
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+        $('#movies').html(output)
+    })
+    .catch((err)=>{
+        console.log(err);
+        
+    })
 }
